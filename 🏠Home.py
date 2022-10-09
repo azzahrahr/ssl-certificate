aaff2fe0386824b0bf7ad3ef223f1b0c3a186fa5
AZZAHRAH <110447871+azzahrahr@users.noreply.github.com>
2022-10-09 10:00:11 +0700

    Create 🏠Home.py

---
+++ 🏠Home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Selamat datang di Website Kami! :wave:")
+st.write("Website ini berisi informasi mengenai SSL Certficate yang dimana ialah sertifikat digital yang mengotentikasi identitas situs web dan memungkinkan koneksi terenkripsi. SSL ialah singkatan dari Secure Sockets Layer, protokol keamanan yang membuat tautan terenkripsi antara server web dan browser web.")
+st.image("images\whyssl.jpg", width = 700)
